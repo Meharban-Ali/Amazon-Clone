3ebd69ab0ab271febc1d9dc24c420e280bf82ea8
Meharban-Ali <meharbanali7900@gmail.com>
2023-12-26 01:04:32 +0530

    fixed file data

--- script.js
+++ script.js
@@ -12,21 +12,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-
-// ==============Scrolling position fixed ====================
-// window.onscroll = function() {myFunction()};
-
-// // Get the header
-// var header = document.getElementById("myHeader");
-
-// // Get the offset position of the navbar
-// var sticky = header.offsetTop;
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.screenY>sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
